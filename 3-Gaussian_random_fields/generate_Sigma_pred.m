5ac5c979e3aeb66fa467b164e0cd8b655059cb3b
den <dennis.d.langer@ntnu.no>
2019-10-04 19:50:34 +0200

    Added equations and desciptions for delivery

--- 3-Gaussian_random_fields/generate_Sigma_pred.m
+++ 3-Gaussian_random_fields/generate_Sigma_pred.m
@@ -4,7 +4,7 @@ eta = theta(2);
 tau2 = theta(3);
 C_0 = zeros(1,m);
 for j = 1:m
-    eucl_dist =norm( s_pred - s_meas(j,:) ); %sqrt((s_pred(1)-s_meas(j,1))^2 + (s_pred(2)-s_meas(j,2))^2)
+    eucl_dist = norm( s_pred - s_meas(j,:) ); %sqrt((s_pred(1)-s_meas(j,1))^2 + (s_pred(2)-s_meas(j,2))^2)
     C_0(1,j) = sigma2 * (1 + eta*eucl_dist) * exp(-eta * eucl_dist);
 end
 

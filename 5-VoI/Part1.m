c982a50016dcc16443a20ef05ae5228b93572a65
Jorge Mendoza <jorge.m.espinosa@ntnu.no>
2019-10-28 15:30:23 +0100

    Update Part1.m

--- 5-VoI/Part1.m
+++ 5-VoI/Part1.m
@@ -1,4 +1,4 @@
-PoV = @(mu,sigma) mu .* normcdf(mu/sigma) + sigma .* normpdf(mu/sigma);
+PoV = @(mu,sigma) mu .* normcdf(mu./sigma) + sigma .* normpdf(mu./sigma);
 
 % i) Range of mu
 mu_plt = -2:0.1:2;
@@ -24,10 +24,23 @@ myeplot
 sigma_plt2 = [0.1,1,2];
 mu_plt2 = -2:0.01:2;
 figure
+subplot(1,2,1)
 hold on
 for j = 1:length(sigma_plt2)
 plot(mu_plt2,PoV(mu_plt2,sigma_plt2(j))-max(0,mu_plt2),'color',[0 0 0]+(j-1)*0.3,'DisplayName',strcat('$\sigma=',num2str(sigma_plt2(j)),'$'));
 legend('interpreter','latex')
 xlabel('$\mu$')
+ylabel('VoI')
+end
+%
+subplot(1,2,2)
+hold on
+sigma_plt3 = 0.01:0.01:2;
+mu_plt3 = [-1,0,2];
+hold on
+for j = 1:length(mu_plt3)
+plot(sigma_plt3,PoV(mu_plt3(j),sigma_plt3)-max(0,mu_plt3(j)),'color',[0 0 0]+(j-1)*0.3,'DisplayName',strcat('$\mu=',num2str(mu_plt3(j)),'$'));
+legend('interpreter','latex')
+xlabel('$\sigma$')
 ylabel('VoI')
 end

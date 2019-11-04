58e3385f75ab9418a66ea74a2bb89b93c216fde5
adallolio <albydallo@gmail.com>
2019-11-04 10:39:41 -0800

    plots finished

--- 5-VoI/Part1.m
+++ 5-VoI/Part1.m
@@ -1,3 +1,8 @@
+close all
+clear
+
+directory = 'plots/part1/';
+
 PoV = @(mu,sigma) mu .* normcdf(mu./sigma) + sigma .* normpdf(mu./sigma);
 
 % i) Range of mu
@@ -13,26 +18,28 @@ PoV_plt(i,j) = PoV(MU_plt(i,j),SIGMA_plt(i,j))-max(0,MU_plt(i,j));
 end
 end
 
-figure(1)
+fig1 = figure;
 surf(MU_plt,SIGMA_plt,PoV_plt)
 grid('on')
-xlabel('$\mu$ ')
-ylabel('$\sigma$ ')
-zlabel('$PoV$')
-myeplot
+xlabel('$\mu$ ','Interpreter','latex', 'FontSize', 11)
+ylabel('$\sigma$ ','Interpreter','latex', 'FontSize', 11)
+zlabel('$PoV$','Interpreter','latex', 'FontSize', 11)
+legend('Interpreter','latex', 'FontSize', 11)
+% title_pic = 'PoV';
+% save_plots(title_pic,directory,fig1);
+
 %%
 sigma_plt2 = [0.1,1,2];
 mu_plt2 = -2:0.01:2;
-figure
+fig2 = figure;
 subplot(1,2,1)
 hold on
 for j = 1:length(sigma_plt2)
 plot(mu_plt2,PoV(mu_plt2,sigma_plt2(j))-max(0,mu_plt2),'color',[0 0 0]+(j-1)*0.3,'DisplayName',strcat('$\sigma=',num2str(sigma_plt2(j)),'$'));
-legend('interpreter','latex')
-xlabel('$\mu$')
+legend('Interpreter','latex', 'FontSize', 11)
+xlabel('$\mu$','Interpreter','latex', 'FontSize', 11)
 ylabel('VoI')
 end
-myeplot
 %
 subplot(1,2,2)
 hold on
@@ -41,8 +48,10 @@ mu_plt3 = [-1,0,2];
 hold on
 for j = 1:length(mu_plt3)
 plot(sigma_plt3,PoV(mu_plt3(j),sigma_plt3)-max(0,mu_plt3(j)),'color',[0 0 0]+(j-1)*0.3,'DisplayName',strcat('$\mu=',num2str(mu_plt3(j)),'$'));
-legend('interpreter','latex')
-xlabel('$\sigma$')
+legend('Interpreter','latex', 'FontSize', 11)
+xlabel('$\sigma$','Interpreter','latex', 'FontSize', 11)
 ylabel('VoI')
 end
-myeplot
+% title_pic = 'figg2';
+% save_plots(title_pic,directory,fig2);
+% myeplot
--- 5-VoI/Part2.m
+++ 5-VoI/Part2.m
@@ -1,5 +1,10 @@
 %% Second task.
 
+close all
+clear
+
+directory = 'plots/part2/';
+
 samples = 1000;
 
 rho_vec = -1:1/(samples/2):1;
@@ -14,10 +19,15 @@ for j=1:samples
     voi2(j) = voi(rho_vec(j),tau_vec(2));
 end
 
-figure
+fig1 = figure;
 plot(rho_vec(2:end)',voi1);
 hold on
 plot(rho_vec(2:end)',voi2);
+xlabel('$\rho$ ','Interpreter','latex', 'FontSize', 11)
+ylabel('$VoI$ ','Interpreter','latex', 'FontSize', 11)
+legend('$\tau=0.5$','$\tau=1$','Interpreter','latex', 'FontSize', 13);
+% title_pic = 'VoI';
+% save_plots(title_pic,directory,fig1);
 
 % d)
 voi_tau_05 = zeros(samples,1);
@@ -27,7 +37,12 @@ for i=1:samples
     voi_tau_1(i) = (1+abs(rho_vec(i)))/sqrt(2*pi*(1+tau_vec(2)^2));
 end
 
-figure
+fig2 = figure;
 plot(rho_vec(2:end)',voi_tau_05);
 hold on
 plot(rho_vec(2:end)',voi_tau_1);
+xlabel('$\rho$ ','Interpreter','latex', 'FontSize', 11)
+ylabel('$VoI$ ','Interpreter','latex', 'FontSize', 11)
+legend('$\tau=0.5$','$\tau=1$','Interpreter','latex', 'FontSize', 13);
+% title_pic = 'VoI2';
+% save_plots(title_pic,directory,fig2);

6289ae853585f2c2c645c9b4647194e95134f418
AndreasVaage <andreas.vage@ntnu.no>
2019-12-04 11:59:48 +0100

    Update plot labels in ex6

--- 6-VoI-MM/main.m
+++ 6-VoI-MM/main.m
@@ -24,17 +24,12 @@ for k=2:N
     marg_p(k)= marg_p(k-1) + 0.05*(1-marg_p(k-1));
     p_x(:,k) = P * p_x(:,k-1);
 end
-figure
-hold on
+figure(1);hold off
+plot(p_x(2,:));hold on
 plot(p_x(1,:))
-plot(p_x(2,:))
-fig1 = figure;
-plot(1:N,marg_p)
-grid
-xlabel('locations')
-ylabel('marg prob')
-title_pic = 'marg_prob';
-%save_plots(title_pic,directory_a,fig1);
+xlabel('railroad section $i$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$p(x_i)$', 'interpreter', 'latex', 'FontSize', 16);
+legend('$p(x_i = 1)$','$p(x_i = 0)$', 'Location', 'best', 'interpreter', 'latex', 'FontSize', 14);
 %% task b
 
 [PV,a] = max([-100000, -5000*sum(p_x(2,:))]);
@@ -51,19 +46,19 @@ y(30) = 0.7;
 v_tau = 100*tau*ones(size(y));
 v_tau(20) = tau;
 v_tau(30) = tau;
-% fig2 = figure;
-% plot(1:data,y);
-% grid
 
 [~,prio_xY,post_xY] = forward_recursion(P,p_x(:,1),v_tau,y,N);
 margp_xY = backward_recursion(P,prio_xY,post_xY,N);
 
-fig3 = figure;
+figure(2);hold off
 plot(1:N,margp_xY(2,:));
+hold on
+plot(1:N,post_xY(2,:),'--');
+plot([20,30],[0.2,0.7],'*');
 grid
-xlabel('locations')
-ylabel('posterior prob for x_i = 1')
-title_pic = 'post_prob';
+xlabel('railroad section $i$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('probability', 'interpreter', 'latex', 'FontSize', 16);
+legend('$p(x_i = 1|\mathbf{y}_D)$','$\mathbf{y}_D$', 'Location', 'best', 'interpreter', 'latex', 'FontSize', 14);
 %save_plots(title_pic,directory_c,fig3);
 
 %% task d
@@ -90,74 +85,73 @@ end
 VoI_mc = PoV - PV;
 maximum = max(VoI_mc)
 max_location = find(VoI_mc==maximum)
-
-fig4 = figure;
+%%
+figure(3); hold off;
 plot(1:N,VoI_mc)
+xlabel('railroad section $k$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('VOI$(k)$', 'interpreter', 'latex', 'FontSize', 16);
+%legend('$p(x_i = 1|\mathbf{y}_D)$','$\mathbf{y}_D$', 'Location', 'best', 'interpreter', 'latex', 'FontSize', 14);
+grid
+
+%% Numerical integration
+% Much faster and more accurate than MC, but currently 10 to low values
+
+%p(y_k | x_k = x) ~ N(x,tau); N(mean, standard_deviation) p(y_k) = sum_{x
+%in {0,1}} ( p(y_k | x_k = x)*p(x_k = x) ) p(y_k) ~ N(0, tau)*p(x_k=0) +
+%N(1, tau)*p(x_k=1)   where p(x_k) >= 0 p(y_k) ~ N(0, tau*p(x_k=0)) +
+%N(p(x_k=1), tau*p(x_k=1)) p(y_k) ~ N(p(x_k=1),sqrt((tau*p(x_k=0))^2 +
+%(tau*p(x_k=1))^2)) p(y_k) ~ N(p(x_k=1),tau*sqrt(p(x_k=0)^2 + p(x_k=1)^2))
+
+Nsamples = 101;
+spread = 4; %standard devitaions 
+mu = p_x(2,:)';
+std = tau*sqrt(sum(p_x.^2,1))';
+step_size = 2*spread*std/(Nsamples-1);
+Ysamples = zeros(N,Nsamples);
+for k = 1:N
+    Ysamples(k,:) = (mu(k)-2*spread*std(k):step_size(k):mu(k)) + spread*std(k);
+end
+% discretized pdf: dpdf_y(k,i) = p(y_k = Ysamples(k,i))
+dpdf_y = normpdf(Ysamples, mu , std);
+
+figure
+hold on
+plot(Ysamples(1,:),dpdf_y(1,:),'*')
+plot(Ysamples(N,:),dpdf_y(N,:),'*')
+grid
+legend("p(y_1)","p(y_N)")
+
+PoV = -ones(N,1);
+for k = 1:k
+    PoV_ni = 0;
+    for i = 1:Nsamples
+        y = normrnd(x,tau,N,1);
+        v_tau = 100*tau*ones(size(y));
+        v_tau(k) = tau;
+        y(k) = Ysamples(k,i);
+        [~,prio_xY,post_xY] = forward_recursion(P, p_x(:,1),v_tau,y,N);
+        total_post_xY = backward_recursion(P,prio_xY,post_xY,N);
+        value_dont_clean = -5000*sum(total_post_xY(2,:));
+        integrand_PoV = max(value_clean_all,value_dont_clean)*dpdf_y(k,i);
+        PoV_ni = PoV_ni + integrand_PoV * step_size(k);
+    end
+    PoV(k) = PoV_ni;
+end
+VoI_ni = PoV - PV;    
+figure
+plot(VoI_ni)
 xlabel('locations')
 ylabel('VoI')
+legend("Numerical integration")
+fig4 = figure;
+hold on
+plot(1:N,VoI_mc)
+plot(VoI_ni)
+legend("Monte carlo","Numerical integration")
 grid
-title_pic = 'VoI_d';
-%save_plots(title_pic,directory_d,fig4);
-
-% %% Numerical integration
-% % Much faster and more accurate than MC, but currently 10 to low values
-% 
-% %p(y_k | x_k = x) ~ N(x,tau); N(mean, standard_deviation) p(y_k) = sum_{x
-% %in {0,1}} ( p(y_k | x_k = x)*p(x_k = x) ) p(y_k) ~ N(0, tau)*p(x_k=0) +
-% %N(1, tau)*p(x_k=1)   where p(x_k) >= 0 p(y_k) ~ N(0, tau*p(x_k=0)) +
-% %N(p(x_k=1), tau*p(x_k=1)) p(y_k) ~ N(p(x_k=1),sqrt((tau*p(x_k=0))^2 +
-% %(tau*p(x_k=1))^2)) p(y_k) ~ N(p(x_k=1),tau*sqrt(p(x_k=0)^2 + p(x_k=1)^2))
-% 
-% Nsamples = 101;
-% spread = 4; %standard devitaions 
-% mu = p_x(2,:)';
-% std = tau*sqrt(sum(p_x.^2,1))';
-% step_size = 2*spread*std/(Nsamples-1);
-% Ysamples = zeros(N,Nsamples);
-% for k = 1:N
-%     Ysamples(k,:) = (mu(k)-2*spread*std(k):step_size(k):mu(k)) + spread*std(k);
-% end
-% % discretized pdf: dpdf_y(k,i) = p(y_k = Ysamples(k,i))
-% dpdf_y = normpdf(Ysamples, mu , std);
-% 
-% figure
-% hold on
-% plot(Ysamples(1,:),dpdf_y(1,:),'*')
-% plot(Ysamples(N,:),dpdf_y(N,:),'*')
-% grid
-% legend("p(y_1)","p(y_N)")
-% 
-% PoV = -ones(N,1);
-% for k = 1:k
-%     PoV_ni = 0;
-%     for i = 1:Nsamples
-%         y = normrnd(x,tau,N,1); % mean = x
-%         v_tau = 100*tau*ones(size(y));
-%         v_tau(k) = tau;
-%         y(k) = Ysamples(k,i);
-%         [~,prio_xY,post_xY] = forward_recursion(P, p_x(:,1),v_tau,y,N);
-%         total_post_xY = backward_recursion(P,prio_xY,post_xY,N);
-%         value_dont_clean = -5000*sum(total_post_xY(2,:));
-%         integrand_PoV = max(value_clean_all,value_dont_clean)*dpdf_y(k,i);
-%         PoV_ni = PoV_ni + integrand_PoV * step_size(k);
-%     end
-%     PoV(k) = PoV_ni;
-% end
-% VoI_ni = PoV - PV;    
-% figure
-% plot(VoI_ni)
-% xlabel('locations')
-% ylabel('VoI')
-% legend("Numerical integration")
-% fig4 = figure;
-% hold on
-% plot(1:N,VoI_mc)
-% plot(VoI_ni)
-% legend("Monte carlo","Numerical integration")
-% grid
-% xlabel('locations')
-% ylabel('VoI')
-% title_pic = 'VoI';
+xlabel('locations')
+ylabel('VoI')
+title_pic = 'VoI';
 % %save_plots(title_pic,directory_d,fig4);
 
 %% task e
@@ -205,17 +199,18 @@ end
 big_voi = big_pov - PV;
 maximum = max(max(big_voi));
 [max_x,max_y]=find(big_voi==maximum)
-
+%%
 fig5 = figure;
 imagesc(big_voi)
 set(gca,'YDir','normal')
-xlabel('locations')
-ylabel('locations')
+xlabel('Sensor Placement 1')
+ylabel('Sensor Placement 2')
 colorbar
 title_pic = 'VoI_cross';
 % save_plots(title_pic,directory_e,fig5);
 
 %% task f
+mc_samples = 10000;
 PoV_f = zeros(mc_samples,1);
 tau_new = 1;
 
@@ -231,11 +226,13 @@ for k=1:mc_samples
 end
 
 VoI_f = PoV_f - PV;
-
+%%
 fig6 = figure;
-plot(1:mc_samples,VoI_f,'*')
-xlabel('locations')
-ylabel('VoI')
+%plot(1:mc_samples,VoI_f,'*')
+histogram(VoI_f)
+set(gca,'YScale','log')
+xlabel('VOI')
+ylabel('# MC samples')
 grid
 title_pic = 'VoI_f';
 % save_plots(title_pic,directory_f,fig6);
